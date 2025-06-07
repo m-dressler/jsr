f4a0931e40d0d846b4e696a37901359d39a4ae64
Mauritz Dressler <mauritz@prodport.com>
2025-06-07 13:07:30 +0000

    📈 versioning.ts | Support `deno.jsonc` files

--- versioning.ts
+++ versioning.ts
@@ -1,5 +1,6 @@
-import { tryParse } from "@std/semver/try-parse";
+import { parse } from "@std/jsonc/";
 import { format } from "@std/semver/format";
+import { tryParse } from "@std/semver/try-parse";
 
 /** All the valid primary versions */
 const primaries = ["major", "minor", "patch"] as const;
@@ -14,20 +15,27 @@ if (!primaries.includes(primary)) {
   Deno.exit(1);
 }
 
-/** The deno.json file as text read from the current directory */
-let denoJsonText = await Deno.readTextFile("./deno.json").catch(() => {
-  console.error('Couldn\'t read "deno.json". Are you in the right directory?');
-  Deno.exit(1);
-});
+const denoTextFiles = await Promise.all(
+  ["./deno.jsonc", "./deno.json"].map(async (path) => ({
+    path,
+    text: await Deno.readTextFile(path).catch(() => {}),
+  })),
+);
 
-/** The JSON parsed deno.json file from the current directory*/
-let denoJson: Record<string, unknown>;
-try {
-  denoJson = JSON.parse(denoJsonText);
-} catch {
-  console.error('"deno.json" isn\t a valid JSON file.');
+/** The deno.json file as text read from the current directory */
+const denoJsonFile = denoTextFiles.find(
+  (v): v is { path: string; text: string } => !!v.text,
+);
+if (!denoJsonFile) {
+  console.error(
+    'Couldn\'t read "deno.jsonc" or "deno.json". Are you in the right directory?',
+  );
   Deno.exit(1);
 }
+
+const denoJson = denoJsonFile.path.endsWith("jsonc")
+  ? parse(denoJsonFile.text)
+  : JSON.parse(denoJsonFile.text);
 
 /** The version string as specified in {@link denoJson} */
 const versionString =
@@ -50,22 +58,25 @@ const updatedVersion = format(version);
 
 // Write back to deno.json. We're not using JSON.stringify so we don't change formatting
 // If we can find the old version string, update it
-if (denoJsonText.includes(`"${versionString}"`)) {
-  denoJsonText = denoJsonText.replace(
+if (denoJsonFile.text.includes(`"${versionString}"`)) {
+  denoJsonFile.text = denoJsonFile.text.replace(
     new RegExp(`("version":\\s*)"${versionString}"`),
     `$1"${updatedVersion}"`,
   );
 } //If it isn't included it, the original semver was missing/invalid so we remove any version key and add versioning back in
 else {
   // Remove any invalid version key
-  denoJsonText = denoJsonText.replace(/\s*"version":\s*"\w*"\s*(,\s*\n)?/, "");
+  denoJsonFile.text = denoJsonFile.text.replace(
+    /\s*"version":\s*"\w*"\s*(,\s*\n)?/,
+    "",
+  );
   // Add the updated version key to the beginning
-  denoJsonText = denoJsonText.replace(
+  denoJsonFile.text = denoJsonFile.text.replace(
     "{",
     `{\n  "version": "${updatedVersion}",`,
   );
 }
-await Deno.writeTextFile("./deno.json", denoJsonText);
+await Deno.writeTextFile(denoJsonFile.path, denoJsonFile.text);
 
 // Create a new commit if a git folder exists
 await Deno.stat(".git/")
